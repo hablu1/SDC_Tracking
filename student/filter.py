029c7aac3be8ec2afeed427702560ff8a8920058
lukas.habermayr@web.de <lukas.habermayr@web.de>
2021-12-30 15:49:10 +0100

    moved from workspace to github

---
+++ student/filter.py
@@ -0,0 +1,129 @@
+# ---------------------------------------------------------------------
+# Project "Track 3D-Objects Over Time"
+# Copyright (C) 2020, Dr. Antje Muntzinger / Dr. Andreas Haja.
+#
+# Purpose of this file : Kalman filter class
+#
+# You should have received a copy of the Udacity license together with this program.
+#
+# https://www.udacity.com/course/self-driving-car-engineer-nanodegree--nd013
+# ----------------------------------------------------------------------
+#
+
+# imports
+import numpy as np
+
+# add project directory to python path to enable relative imports
+import os
+import sys
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+import misc.params as params 
+
+class Filter:
+    '''Kalman filter class'''
+    def __init__(self):
+        self.dim_state = params.dim_state # process model dimension
+        self.dt = params.dt # time increment
+        self.q= params.q # process noise variable for Kalman filter Q
+
+    def F(self):
+        ############
+        # TODO Step 1: implement and return system matrix F
+        ############
+            
+        # system matrix
+        dt = self.dt
+        return np.matrix([[1, 0, 0, dt, 0, 0],
+                        [0, 1, 0, 0, dt, 0 ],
+                        [0, 0, 1, 0, 0, dt],
+                        [0, 0, 0, 1, 0, 0 ],
+                        [0, 0, 0, 0, 1, 0 ],
+                        [0, 0, 0, 0, 0, 1 ]])
+
+        
+        
+        ############
+        # END student code
+        ############ 
+
+    def Q(self):
+        ############
+        # TODO Step 1: implement and return process noise covariance Q
+        ############
+        dt = params.dt
+        Q = np.zeros((params.dim_state, params.dim_state))
+        np.fill_diagonal(Q, dt* params.q)
+
+        return np.matrix(Q)
+        
+        ############
+        # END student code
+        ############ 
+
+    def predict(self, track):
+        ############
+        # TODO Step 1: predict state x and estimation error covariance P to next timestep, save x and P in track
+        ############
+        F = self.F()
+        x = F*track.x # state prediction
+        P = F*track.P*F.transpose() + self.Q() # covariance prediction
+        
+        track.set_x(x)
+        track.set_P(P)
+        
+        #return x, P
+        
+        ############
+        # END student code
+        ############ 
+
+    def update(self, track, meas):
+        ############
+        # TODO Step 1: update state x and covariance P with associated measurement, save x and P in track
+        ############
+        # update state and covariance with associated measurement
+        H = meas.sensor.get_H(track.x) # measurement matrix
+        res = self.gamma(track, meas)
+        S = self.S(track, meas, H) # covariance of residual
+        K = track.P*H.transpose()*np.linalg.inv(S) # Kalman gain
+        x = track.x + K*res # state update
+        I = np.identity(self.dim_state)
+        P = (I - K*H) * track.P # covariance update
+        
+        track.set_P(P)
+        track.set_x(x)
+        
+        #return x, P 
+        
+        ############
+        # END student code
+        ############ 
+        track.update_attributes(meas)
+    
+    def gamma(self, track, meas):
+        ############
+        # TODO Step 1: calculate and return residual gamma
+        ############
+                
+        gamma = meas.z - meas.sensor.get_hx(track.x) # residual
+
+        return gamma
+        
+        ############
+        # END student code
+        ############ 
+
+    def S(self, track, meas, H):
+        ############
+        # TODO Step 1: calculate and return covariance of residual S
+        ############
+        
+        S = H*track.P*H.transpose() + meas.R # covariance of residual
+
+        return S
+        
+        ############
+        # END student code
+        ############ 
